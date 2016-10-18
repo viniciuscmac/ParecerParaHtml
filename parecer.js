a638f64885a59dbe6950d062835818a5de046089
viniciuscmac <viniciuscmac@gmail.com>
2016-10-18 09:45:53 -0200

    Novo layout

--- parecer.js
+++ parecer.js
@@ -16,7 +16,7 @@ function json(){
 		$.getJSON('arquivo.json', function(data){
 			this.qtd = data.length;
 			this.retorno = '<table align="center"><tr> <th>Atributo</th> <th>Resultado</th></tr>';
-			this.retorno += '<tr><td>ID: </td><td>' + data.id + '</td></tr>';						
+			this.retorno += '<tr><td>ID: </td><td>' + data.id + '</td></tr>';
 			this.retorno +='<tr><td>(Art. 39) Total de meses: </td><td>' +data.totalMeses + '</td></tr>';
 			this.retorno +='<tr><td>(Art. 39) Verifica numero de meses: </td><td>' +data.meses24 + '</td></tr>';
 			this.retorno +='<tr><td>(Art. 39) Somatório ponderado: </td><td>' +data.somaPonderada + '</td></tr>';
@@ -28,25 +28,25 @@ function json(){
 			this.retorno +='<tr><td>Inciso V (Art. 40): </td><td>' +data.aprovadoChefia + '</td></tr>';
 			this.retorno +='<tr><td>Inciso VI (Art. 40): </td><td>' +data.inciso6 + '</td></tr>';
 			this.retorno +='<tr><td>Habilitação parcial (Art. 40): </td><td>' +data.habilitacaoParcial + '</td></tr>';
-			this.retorno +='<tr><td>Habilitação para caso especial de estágio (Art. 40, parágrafo primeiro): </td><td>' 
+			this.retorno +='<tr><td>Habilitação para caso especial de estágio (Art. 40, parágrafo primeiro): </td><td>'
 			+data.habilitacaoEstagio + '</td></tr>';
-			this.retorno +='<tr><td>Habilitação para caso especial de cargo de direção ou pós-graduação (Art. 40 parágrafo segundo): </td><td>' 
+			this.retorno +='<tr><td>Habilitação para caso especial de cargo de direção ou pós-graduação (Art. 40 parágrafo segundo): </td><td>'
 			+data.habilitacaoCargoOuPos + '</td></tr>';
-			
+
 			if (data.habilitado == "sim"){
 			this.retorno +='<tr><td>Habilitado para a promoção: </td><td>' +data.habilitado + ' PARABENS!!!</td></tr>';
 			}
 			else{
 			this.retorno +='<tr><td>Habilitado para a promoção: </td><td>' +data.habilitado + '</td></tr>';
 			}
-			
+
 			this.retorno +='<tr><td>Soma de ensino: </td><td>' +data.somaEnsino + '</td></tr>';
 			this.retorno +='<tr><td>Soma pesquisa e extensão: </td><td>' +data.somaPesquisa + '</td></tr>';
 			this.retorno +='<tr><td>Soma administração e representação: </td><td>' +data.somaAdministracao + '</td></tr>';
 			this.retorno +='<tr><td>Soma outras: </td><td>' +data.somaOutras + '</td></tr>';
-			this.retorno +='<tr><td>Soma de todos os pontos exceto aqueles de produção intelectual: </td><td>' 
+			this.retorno +='<tr><td>Soma de todos os pontos exceto aqueles de produção intelectual: </td><td>'
 			+data.S + '</td></tr></table>';
-	
+
 			/*for (i = 0; i < this.qtd; i++){
 				//this.retorno += data.parecer[i] + '<br />';
 				this.retorno += 'ID: ' + data.parecer[i].id + '<br />';
@@ -60,5 +60,3 @@ function json(){
 	}
 
 }
-
-
