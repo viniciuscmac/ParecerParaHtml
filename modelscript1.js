511e5add266d3556161f1204136e9cfde5aa6cc7
NIKOLAS\Nikolas <nikolasmatiasxaviermorais@hotmail.com>
2016-11-04 20:54:29 -0200

    Foi retirado o CSS de dentro do arquivo javascript e colocado no arquivo modelo1.css. Além disso, esse mesmo arquivo foi colocado como link no model1.html.

--- modelscript1.js
+++ modelscript1.js
@@ -20,28 +20,28 @@ function json(){
             this.qtd = data.length;
             this.retorno = '<table align="center" class="table table-bordered table-striped table-hover">';
             this.retorno += '<tr><th colspan="2" class="text-center">ID: ' + data.id + ' </th></tr>';
-            this.retorno +='<tr><td rowspan="4"><span style="font-size: 10px">(Art. 39) Nota final:</span> <div>' + data.notaFinal +
-                '</div><p style="font-size: 10px; float: right">Inciso VI (Art. 40): ' + data.inciso6 + '</p></td></tr>';
-            this.retorno +='<tr><td><span style="font-size: 10px">(Art. 39) Total de meses:</span> <div>' +data.totalMeses + '</div></td></tr>';
-            this.retorno +='<tr><td><span style="font-size: 10px">(Art. 39) Verifica numero de meses: </span><div>' +data.meses24 + '</div></td></tr>';
-            this.retorno +='<tr><td><span style="font-size: 10px">(Art. 39) Somatório ponderado:</span><div> ' +data.somaPonderada + '</div></td></tr>';
+            this.retorno +='<tr><td rowspan="4"><span class="pequenosTitulos">(Art. 39) Nota final:</span> <div>' + data.notaFinal +
+                '</div><p class="pequenasLaterais">Inciso VI (Art. 40): ' + data.inciso6 + '</p></td></tr>';
+            this.retorno +='<tr><td><span class="pequenosTitulos">(Art. 39) Total de meses:</span> <div>' +data.totalMeses + '</div></td></tr>';
+            this.retorno +='<tr><td><span class="pequenosTitulos">(Art. 39) Verifica numero de meses: </span><div>' +data.meses24 + '</div></td></tr>';
+            this.retorno +='<tr><td><span class="pequenosTitulos">(Art. 39) Somatório ponderado:</span><div> ' +data.somaPonderada + '</div></td></tr>';
             this.retorno +='<tr><td rowspan="4"><span style="font-size: 10px">Habilitado para a promoção:</span><div>' +data.habilitado + '</div></td></tr>';
-            this.retorno +='<tr><td><span style="font-size: 10px">Habilitação parcial (Art. 40): </span><div>' +data.habilitacaoParcial + '</div>' +
-                '<p style="font-size:10px;float:right">Inciso II (Art. 40): ' + data.inciso2 +'</p></td></tr>';
-            this.retorno +='<tr><td><span style="font-size: 10px">Habilitação para caso especial de estágio (Art. 40, parágrafo primeiro): </span><div>'
+            this.retorno +='<tr><td><span class="pequenosTitulos">Habilitação parcial (Art. 40): </span><div>' +data.habilitacaoParcial + '</div>' +
+                '<p class="pequenasLaterais">Inciso II (Art. 40): ' + data.inciso2 +'</p></td></tr>';
+            this.retorno +='<tr><td><span class="pequenosTitulos">Habilitação para caso especial de estágio (Art. 40, parágrafo primeiro): </span><div>'
                 +data.habilitacaoEstagio + '</div></td></tr>';
-            this.retorno +='<tr><td><span style="font-size: 10px">Habilitação para caso especial de cargo de direção ou pós-graduação (Art. 40 parágrafo segundo): </span><div>'
+            this.retorno +='<tr><td><span class="pequenosTitulos">Habilitação para caso especial de cargo de direção ou pós-graduação (Art. 40 parágrafo segundo): </span><div>'
                 +data.habilitacaoCargoOuPos + '</div></td></tr>';
-            this.retorno +='<tr><td rowspan="5"><span style="font-size: 10px">Soma de todos os pontos exceto aqueles de produção intelectual:</span><div>'
+            this.retorno +='<tr><td rowspan="5"><span class="pequenosTitulos">Soma de todos os pontos exceto aqueles de produção intelectual:</span><div>'
                 +data.S + '</div></td></tr>';
-            this.retorno +='<tr><td><span style="font-size: 10px">Soma de ensino:</span><div> ' +data.somaEnsino +
-                '</div><p style="font-size:10px;float:right">Inciso I (Art. 40): ' + data.inciso1 +'</p></td></tr>';
-            this.retorno +='<tr><td><span style="font-size: 10px">Soma pesquisa e extensão:</span><div> ' +data.somaPesquisa +
-                '</div><p style="font-size:10px;float:right">Inciso III (Art. 40): ' + data.inciso3 +'</p></td></tr>';
-            this.retorno +='<tr><td><span style="font-size: 10px">Soma administração e representação:</span><div> ' +data.somaAdministracao +
-                '</div><p style="font-size:10px;float:right">Inciso IV (Art. 40): ' + data.inciso4 +'</p></td></tr>';
-            this.retorno +='<tr><td><span style="font-size: 10px">Soma outras: </span><div>' +data.somaOutras +
-                '</div><p style="font-size:10px;float:right">Inciso V (Art. 40): ' + data.aprovadoChefia +'</p></td></tr></table>';
+            this.retorno +='<tr><td><span class="pequenosTitulos">Soma de ensino:</span><div> ' +data.somaEnsino +
+                '</div><p class="pequenasLaterais">Inciso I (Art. 40): ' + data.inciso1 +'</p></td></tr>';
+            this.retorno +='<tr><td><span class="pequenosTitulos">Soma pesquisa e extensão:</span><div> ' +data.somaPesquisa +
+                '</div><p class="pequenasLaterais">Inciso III (Art. 40): ' + data.inciso3 +'</p></td></tr>';
+            this.retorno +='<tr><td><span class="pequenosTitulos">Soma administração e representação:</span><div> ' +data.somaAdministracao +
+                '</div><p class="pequenasLaterais">Inciso IV (Art. 40): ' + data.inciso4 +'</p></td></tr>';
+            this.retorno +='<tr><td><span class="pequenosTitulos">Soma outras: </span><div>' +data.somaOutras +
+                '</div><p class="pequenasLaterais">Inciso V (Art. 40): ' + data.aprovadoChefia +'</p></td></tr></table>';
 
             /*for (i = 0; i < this.qtd; i++){
              //this.retorno += data.parecer[i] + '<br />';
